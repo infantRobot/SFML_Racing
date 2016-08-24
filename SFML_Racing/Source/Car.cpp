40952c03ae90ed26a2465179d1ba0ab2025deda8
Alexander Medeiros <Alexander Medeiros>
2016-08-24 15:34:07 -0800

    Adding Physics stuff

--- SFML_Racing/Source/Car.cpp
+++ SFML_Racing/Source/Car.cpp
@@ -20,7 +20,6 @@ Car::Car()
 	m_draw.m_rect.setSize(m_carSize);
 	m_draw.m_rect.setOrigin(sf::Vector2f(m_carSize.x / 2, m_carSize.y / 2));
 
-	m_phys.SetFric(4);
 
 	UpdateCar(0);
 }
@@ -34,15 +33,9 @@ void Car::UpdateCar(float dt)
 	m_position += m_phys.UpdatePos(dt);
 	float varX = (m_position.x - oldPos.x);
 	float varY = (m_position.y - oldPos.y);
-	if (varX == 0)
-	{
-		//tan not defined for x=0
-		m_angle = 0;
-	}
-	else
-	{
-		m_angle = tan(varY / varX);
-	}
+	
+	m_angle = 0;//m_phys.UpdateAng(dt)*180/3.14159;
+	
 
 	//m_draw.m_rect.setPosition(m_position);
 	//Update Drawable
@@ -51,15 +44,15 @@ void Car::UpdateCar(float dt)
 	//Get Inputs
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
 	{
-		m_phys.XForce(acceleration);
+		m_phys.DriveForce(acceleration);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 	{
-		m_phys.XForce(-acceleration);
+		m_phys.DriveForce(-acceleration);
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 	{
-		m_phys.YForce(3);
+		m_phys.TurnCar(30);
 	}
 	else
 		m_angle = 0;
--- SFML_Racing/Source/Physics.hpp
+++ SFML_Racing/Source/Physics.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 //physics works on a local axis
 //x is forward of the object
@@ -8,33 +9,56 @@
 class Physics
 {
 public:
-	void XForce(float var) { xAcc += var; }
-	void YForce(float var) { yAcc += var; }
 
-	void SetFric(float fric) { friction = fric; }
-
-	sf::Vector2f UpdatePos(float dt)
+	Physics()
 	{
-		if (xAcc > 0)
-			xAcc -= friction;
-		if (xAcc < 0)
-			xAcc += friction;
-
-		xSpd = xAcc * dt;
-		ySpd = yAcc * dt;
-		sf::Vector2f vec(xSpd, ySpd);
-		return vec;
+		m_pos = sf::Vector2f(0,0);
 	}
-	sf::Vector2f UpdateAng(float dt)
-	{
 
-	}
+	void TurnCar(float ang);
+	void DriveForce(float force);
+	
+	sf::Vector2f UpdatePos(float dt);
+	float UpdateAng(float dt);
+
 private:
+	float ForceTraction();
+	float ForceDrive();
+	float ForceBraking();
+	float ForceDrag();
+	float ForceRR();
+
+	//Speed of the car
+	float Velocity() { return sqrt(m_vel.x*m_vel.x + m_vel.y*m_vel.y); }
+	float Angle() { if (m_vel.x == 0) return PI/2; else return atan(m_vel.y / m_vel.x); }
+
+private:
+	sf::Vector2f m_acc;
+	sf::Vector2f m_vel;
+	sf::Vector2f m_pos;
+
+	float m_steerAngle = 0;
+
+	//Variables for drive force
+	float m_engTorque = 450; //N*m
+	float m_gearRatio = 2.66;
+	float m_diffRatio = 3.42;
+	float m_tranEff = 0.7;
+	float const M_WHEELR = 0.34; // m
+	float const M_WHEELBASE = 2; // m
+
+	//Constants for drag coef
+	float const m_mass = 1500; //kg
+	float const C_DRAG = 0.30; //u
+	float const M_AREA = 2.2; //m^2
+	float const RHO_AIR = 1.29; //kg/m^3
+
+	float const C_BRAKE = 20;
+
+	float const PI = 3.1415926535;
 
 	float xAcc = 0;
 	float yAcc = 0;
 	float xSpd = 0;
 	float ySpd = 0;
-
-	float friction = 2;
 };
